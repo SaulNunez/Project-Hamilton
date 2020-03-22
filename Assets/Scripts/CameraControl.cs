3df4c3c236909367fcedb27c2299c30ac8b68ee3
Saúl Núñez <saul.nunez99@gmail.com>
2020-03-21 18:11:45 -0600

    Un script para controlar la camara para que siga al jugador.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject character;
+    private Vector3 target;
+    public float movementSpeed = 0.5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        target.x = character.transform.position.x;
+        target.y = character.transform.position.y;
+        target.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, target, movementSpeed * Time.deltaTime);
+    }
+}
