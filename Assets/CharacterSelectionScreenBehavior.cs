d9ef539fb0c4fabc117578a5f1e7c0135c562353
Saul Nuñez <saul.nunez99@gmail.com>
2020-08-12 13:02:25 -0600

    Iniciar sockets correctamente en todos los navegadores incluyendo Firefox

---
+++ Assets/CharacterSelectionScreenBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectionScreenBehavior : MonoBehaviour
+{
+    public GameObject playerSelectionScreen;
+
+    void Start()
+    {
+        Socket.Instance.LobbyJoinedEvent += OpenPlayerSelection;
+    }
+
+    private void OnDestroy()
+    {
+        Socket.Instance.LobbyJoinedEvent -= OpenPlayerSelection;
+    }
+
+    private void OpenPlayerSelection(LobbyJoinedData data, string error = null)
+    {
+        if(error == null)
+        {
+            playerSelectionScreen.SetActive(true);
+            enabled = false;
+        }
+    }
+}
