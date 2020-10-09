b350ed8bff5f8118d0c14f368dfc27faeff0a8d4
Saul Nuñez <saul.nunez99@gmail.com>
2020-10-08 19:50:19 -0600

    Acepta peticiones para mostrar puzzles y manda trabajo del jugador al servidor

---
+++ Assets/Scripts/Multiplayer/ResultPayload/PuzzleResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Multiplayer.ResultPayload
+{
+    public class PuzzleResult
+    {
+        public string Output { get; set; }
+        public bool Correct { get; set; }
+    }
+}
