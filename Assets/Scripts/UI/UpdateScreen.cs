db27b18749c59bd92ec2b55fc78cc79bfa625182
Saul Nuñez <saul.nunez99@gmail.com>
2020-11-21 01:53:36 -0700

    Mostrar stats en pantalla

--- Assets/Scripts/UI/UpdateScreen.cs
+++ Assets/Scripts/UI/UpdateScreen.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpdateScreen : MonoBehaviour
 {
-    public void RefreshPlayerStats()
-    {
+    public Text statBraveryText;
+    public Text statIntelligenceText;
+    public Text statPhysicalText;
+    public Text statSanityText;
 
+    private void Start()
+    {
+        HamiltonHub.Instance.OnStatsUpdate += StatsUpdate;
+    }
+
+    private void StatsUpdate(Assets.Scripts.Multiplayer.ServerRequestsPayload.NewStats newStats)
+    {
+        if(newStats.PlayerName == HamiltonHub.Instance.SelectedCharacter)
+        {
+            statBraveryText.text = newStats.Stats.Bravery.ToString();
+            statIntelligenceText.text = newStats.Stats.Intelligence.ToString();
+            statPhysicalText.text = newStats.Stats.Physical.ToString();
+            statSanityText.text = newStats.Stats.Sanity.ToString();
+        }
     }
 }
