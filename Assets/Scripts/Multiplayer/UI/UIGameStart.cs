f57a329eece5d453cd65234623a657bfcbbe1a04
Saul Nunez <saul.nunez99@gmail.com>
2020-12-18 20:53:34 -0700

    UI para lobby

---
+++ Assets/Scripts/Multiplayer/UI/UIGameStart.cs
@@ -0,0 +1,17 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameStart : NetworkBehaviour
+{
+    public LobbyNetworkManager lobbyNetworkManager;
+
+    public void StartGame()
+    {
+        if (isServer)
+        {
+            lobbyNetworkManager.ServerChangeScene(lobbyNetworkManager.gameStart);
+        }
+    }
+}
---
+++ Assets/Scripts/Multiplayer/UI/UiPlayerCountUpdate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TextCore;
+
+public class UiPlayerCountUpdate : MonoBehaviour
+{
+    LobbyNetworkManager lobbyNetworkManager;
+    TextMeshProUGUI textbox;
+
+    private void UpdateScreen()
+    {
+        textbox.text = $"{lobbyNetworkManager.numPlayers}/{lobbyNetworkManager.maxConnections}";
+    }
+
+    void Start()
+    {
+        textbox = GetComponent<TextMeshProUGUI>();
+
+        LobbyNetworkManager.OnClientConnected += UpdateScreen;
+
+        var nmGameObject = GameObject.FindGameObjectWithTag("NetworkManager");
+        lobbyNetworkManager = nmGameObject.GetComponent<LobbyNetworkManager>();
+
+        UpdateScreen();
+    }
+
+    void OnDestroy()
+    {
+        LobbyNetworkManager.OnClientConnected -= UpdateScreen;
+    }
+}
