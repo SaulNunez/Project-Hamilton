81b29ac8d88ef7516e225bfa6da27947ca99f724
Saul Nunez <saul.nunez99@gmail.com>
2020-12-23 19:27:49 -0700

    Actualizar Mirror y pantalla de chat

---
+++ Assets/Scripts/PlayersChat.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Mirror;
+using System;
+
+public class PlayersChat : NetworkBehaviour
+{
+    [SerializeField] 
+    private TMP_Text chatBox;
+    [SerializeField] 
+    private TMP_InputField messageBox;
+
+    public static event Action<string, string> onNewMessage;
+
+    public override void OnStartAuthority()
+    {
+        onNewMessage += PrintOnScreen;
+    }
+
+    public void PrintOnScreen(string userName, string message)
+    {
+        messageBox.text += $"[{userName}]: {message}\n";
+    }
+
+    [Client]
+    public void SendMessage()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            CmdTransmitMessage("a",messageBox.text);
+            //Clean textbox;
+            messageBox.text = string.Empty;
+        }
+    }
+
+    [Command]
+    public void CmdTransmitMessage(string userName, string message)
+    {
+        PrintMessage(userName, message);
+    }
+
+    [ClientRpc]
+    public void PrintMessage(string userName, string message)
+    {
+        onNewMessage?.Invoke(userName, message);
+    }
+
+    public override void OnStopAuthority()
+    {
+        onNewMessage -= PrintOnScreen;
+    }
+}
