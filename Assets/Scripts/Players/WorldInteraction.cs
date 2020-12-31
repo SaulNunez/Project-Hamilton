7dbae474d3719afdb73308910c92ec713243b69d
Saul Nunez <saul.nunez99@gmail.com>
2020-12-31 00:21:39 -0700

    Detectar gameobjects cercanos que se puedan interactuar con

---
+++ Assets/Scripts/Players/WorldInteraction.cs
@@ -0,0 +1,55 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInteraction : NetworkBehaviour
+{
+    public delegate void ObjectStartDetected();
+    public event ObjectStartDetected OnObjectStartDetected;
+
+    public delegate void ObjectEndDetected();
+    public event ObjectEndDetected OnObjectEndDetected;
+
+    bool objectNear = false;
+
+    [SerializeField]
+    LayerMask taskMask;
+
+    void Update()
+    {
+        bool somethingNear = Physics2D.OverlapCircle(transform.position, 3f, taskMask);
+
+        if(somethingNear != objectNear)
+        {
+            if (somethingNear)
+            {
+                OnObjectStartDetected?.Invoke();
+            } else
+            {
+                OnObjectEndDetected?.Invoke();
+            }
+        }
+        objectNear = somethingNear;
+    }
+
+    public void InteractWithEnvironment()
+    {
+        if (objectNear)
+        {
+            CmdInteractWithNearObject();
+        }
+    }
+
+    [Server]
+    public void CmdInteractWithNearObject()
+    {
+        Collider2D somethingNear = Physics2D.OverlapCircle(transform.position, 3f, taskMask);
+        var interactuables = somethingNear.GetComponents<InteractuableBehavior>();
+
+        foreach(var interactuable in interactuables)
+        {
+            interactuable.OnApproach(gameObject);
+        }
+    }
+}
