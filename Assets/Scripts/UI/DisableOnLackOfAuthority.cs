ea8e6ea91e56fa14791d70b4936a2c19101e3d75
Saul Nunez <saul.nunez99@gmail.com>
2021-01-02 21:49:23 -0700

    UI de gameplay

---
+++ Assets/Scripts/UI/DisableOnLackOfAuthority.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnLackOfAuthority : NetworkBehaviour
+{
+    void Start()
+    {
+        if (!hasAuthority)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
