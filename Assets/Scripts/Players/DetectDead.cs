32316fa2a50f0f4670a5d41f35ba0db45f8875df
Saul Nunez <saul.nunez99@gmail.com>
2021-01-26 16:44:19 -0700

    WIP votacion de asesinos

---
+++ Assets/Scripts/Players/DetectDead.cs
@@ -0,0 +1,62 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectDead : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(DetectedDeadPlayerChanged))]
+    private bool detected;
+
+    private void DetectedDeadPlayerChanged(bool oldValue, bool newValue)
+    {
+        
+    }
+
+    [SerializeField]
+    private LayerMask deadPlayersLayers;
+
+    HubConfig config;
+
+    Collider2D other;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        var hubConfigGO = GameObject.FindGameObjectWithTag(Tags.HubConfig);
+        config = hubConfigGO.GetComponent<HubConfig>();
+    }
+
+    void Update()
+    {
+        if (isServer)
+        {
+            var found = Physics2D.OverlapCircle(transform.position, config.actDistance, deadPlayersLayers);
+            if (found)
+            {
+                other = found;
+                detected = true;
+            } else
+            {
+                detected = false;
+            }
+        }
+    }
+
+    [Command]
+    public void CmdReport()
+    {
+        if (detected)
+        {
+
+        }
+    }
+
+    [ClientRpc]
+    public void RpcStartVoting()
+    {
+
+    }
+}
