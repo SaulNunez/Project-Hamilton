9e4ce90fef151f5e73b30a17f308f6250f838fee
Saul Nunez <saul.nunez99@gmail.com>
2021-02-27 04:00:03 -0700

    (WIP) Puzzle bool

---
+++ Assets/Scripts/Puzzles/VariableBool/CompleteBoolPuzzle.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteBoolPuzzle : NetworkBehaviour
+{
+    public void CompletePuzzle(bool toggleState)
+    {
+        if (toggleState)
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Rooms/OpenPuzzle.cs
+++ Assets/Scripts/Rooms/OpenPuzzle.cs
@@ -7,7 +7,8 @@ using System.Linq;
 
 public class OpenPuzzle : InteractuableBehavior
 {
-    HubConfig hubConfig;
+    [SyncVar]
+    public bool puzzleIsAvailable = true;
 
     [SerializeField]
     LayerMask playerLayer;
@@ -15,19 +16,16 @@ public class OpenPuzzle : InteractuableBehavior
     [SerializeField]
     PuzzleId opens;
 
-    public override void OnStartServer()
-    {
-        var lobbyConfigs = GameObject.FindGameObjectWithTag(Tags.HubConfig);
-        hubConfig = lobbyConfigs.GetComponent<HubConfig>();
-    }
-
     [Server]
     public override void OnApproach(GameObject approachedBy)
     {
-        print("Show puzzle");
-        //TODO: Crear id de puzzle en el servidor para mantener el estado
-        //TargetOpenPuzzleOnPlayer(approachedBy.GetComponent<NetworkTransform>().connectionToClient);
-        var puzzleManager = approachedBy.GetComponent<ShowPuzzle>();
-        puzzleManager.OpenPuzzles(opens);
+        if (puzzleIsAvailable)
+        {
+            print("Show puzzle");
+            //TODO: Crear id de puzzle en el servidor para mantener el estado
+            //TargetOpenPuzzleOnPlayer(approachedBy.GetComponent<NetworkTransform>().connectionToClient);
+            var puzzleManager = approachedBy.GetComponent<ShowPuzzle>();
+            puzzleManager.OpenPuzzles(opens);
+        }
     }
 }
