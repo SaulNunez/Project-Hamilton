8c4f8bcbdd7718fdc5470deaf7692272c9705485
Saul Nunez <saul.nunez99@gmail.com>
2021-02-27 01:20:56 -0700

    Esconder puzzles al inciar el juego con un workaround

---
+++ Assets/Scripts/UI/FixActiveStateOnStart.cs
@@ -0,0 +1,18 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Workaround on failing to passing gameobject active property when spawning UI on client.
+/// Disables UI to prevent poluding the screen on the user.
+/// </summary>
+public class FixActiveStateOnStart : NetworkBehaviour
+{
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        gameObject.SetActive(false);
+    }
+}
