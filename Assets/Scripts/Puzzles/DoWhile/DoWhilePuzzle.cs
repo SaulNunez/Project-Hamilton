0411a25f6cd17a8a9d199b323381c5633487fefb
Saul Nunez <saul.nunez99@gmail.com>
2021-03-01 16:37:45 -0700

    Puzzle DoWhile

---
+++ Assets/Scripts/Puzzles/DoWhile/DoWhilePuzzle.cs
@@ -0,0 +1,24 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Logic for do while puzzle
+/// </summary>
+public class DoWhilePuzzle : NetworkBehaviour
+{
+    public void CheckTextboxInput(string text)
+    {
+        CmdCheckResultInServer(text);
+    }
+
+    [Command]
+    void CmdCheckResultInServer(string text)
+    {
+        if(text.ToLower() == "verdadero")
+        {
+            PuzzleCompletion.instance.MarkCompleted(PuzzleId.DoWhileMotorStarter);
+        }
+    }
+}
