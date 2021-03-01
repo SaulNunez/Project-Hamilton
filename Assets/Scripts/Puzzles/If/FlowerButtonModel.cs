0c51b793f411014e0d0fef851f7e2b0b901f82dc
Saul Nunez <saul.nunez99@gmail.com>
2021-02-28 22:07:05 -0700

    Puzzle if, agregar solo mostrar a owner a los diferentes puzzles

---
+++ Assets/Scripts/Puzzles/If/FlowerButtonModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FlowerButtonModel : MonoBehaviour
+{
+    public Button button;
+
+    [SerializeField]
+    Image sprite;
+
+    public Sprite ButtonSprite { 
+        get => sprite.sprite; 
+        set => sprite.sprite = value; 
+    }
+}
--- Assets/Scripts/Puzzles/If/SetupDefaultFlowers.cs
+++ Assets/Scripts/Puzzles/If/SetupDefaultFlowers.cs
@@ -1,11 +1,53 @@
 ﻿using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetupDefaultFlowers: NetworkBehaviour {
-    [SyncVar]
+    [SyncVar(hook = nameof(OnDefaultFlowerSet))]
     string defaultFlowerType;
+
+    [SerializeField]
+    Image defaultFlowerImage;
+
+    [Header("Sprites")]
+    [SerializeField]
+    Sprite sunflowerSprite;
+
+    [SerializeField]
+    Sprite daisySprite;
+
+    [SerializeField]
+    Sprite tulipSprite;
+
+    [SerializeField]
+    Sprite rosesSprite;
+
+    [Header("Buttons")]
+    [SerializeField]
+    List<FlowerButtonModel> flowerButtons;
+
+
+    void OnDefaultFlowerSet(string oldValue, string newValue)
+    {
+        switch (newValue)
+        {
+            case FlowerTypes.Sunflower:
+                defaultFlowerImage.sprite = sunflowerSprite;
+                break;
+            case FlowerTypes.Tulip:
+                defaultFlowerImage.sprite = tulipSprite;
+                break;
+            case FlowerTypes.Roses:
+                defaultFlowerImage.sprite = rosesSprite;
+                break;
+            case FlowerTypes.Daisy:
+                defaultFlowerImage.sprite = daisySprite;
+                break;
+        }
+    }
 
     public override void OnStartServer()
     {
@@ -13,6 +55,42 @@ public class SetupDefaultFlowers: NetworkBehaviour {
 
         defaultFlowerType = FlowerTypes.Types.PickRandom();
 
-        var buttons = FlowerTypes.Types.PickRandom(6);
+        var buttonFlower = FlowerTypes.Types.PickRandom(6).ToList();
+        SetupOnClient(buttonFlower);
+    }
+
+    [Command]
+    void CmdOnButtonClick(string clickFlowerType)
+    {
+        if(clickFlowerType == defaultFlowerType)
+        {
+            PuzzleCompletion.instance.MarkCompleted(PuzzleId.IfFlowerPicking);
+        }
+    }
+
+    [ClientRpc]
+    void SetupOnClient(List<string> flowerTypes)
+    {
+        for(int i = 0; i < flowerButtons.Count; i++)
+        {
+            switch (flowerTypes[i])
+            {
+                case FlowerTypes.Sunflower:
+                    flowerButtons[i].ButtonSprite = sunflowerSprite;
+                    break;
+                case FlowerTypes.Tulip:
+                    flowerButtons[i].ButtonSprite = tulipSprite;
+                    break;
+                case FlowerTypes.Roses:
+                    flowerButtons[i].ButtonSprite = rosesSprite;
+                    break;
+                case FlowerTypes.Daisy:
+                    flowerButtons[i].ButtonSprite = daisySprite;
+                    break;
+            }
+
+            //En click, mandar seleccion al servidor
+            flowerButtons[i].button.onClick.AddListener(() => CmdOnButtonClick(flowerTypes[i]));
+        }
     }
 }
