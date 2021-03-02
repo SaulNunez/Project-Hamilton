e1b2effaf57ffc8ffeef727067e6f1753b96067a
Saul Nunez <saul.nunez99@gmail.com>
2021-03-01 20:57:44 -0700

    While puzzle

---
+++ Assets/Scripts/Puzzles/WhilePuzzle/WhilePuzzle.cs
@@ -0,0 +1,32 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhilePuzzle : NetworkBehaviour
+{
+    public const int defaultValue = 6;
+
+    public void CheckInput(string input)
+    {
+        try
+        {
+            var turnsInput = int.Parse(input);
+            CmdCheckInput(turnsInput);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    [Command]
+    void CmdCheckInput(int input)
+    {
+        if(input == defaultValue)
+        {
+            PuzzleCompletion.instance.MarkCompleted(PuzzleId.WhileFillingBucket);
+        }
+    }
+}
