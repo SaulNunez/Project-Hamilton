18ef95f8978aa10f09a189a590c7cdfe40f76a5e
Saul Nunez <saul.nunez99@gmail.com>
2021-03-02 22:50:21 -0700

    Sabotage de boiler

---
+++ Assets/Scripts/Puzzles/SabotageBoilers/BoilerSabotageOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class BoilerSabotageOptions
+{
+    public Sprite gauge;
+    public int maximum;
+    public int minimum;
+}
---
+++ Assets/Scripts/Puzzles/SabotageBoilers/SabotageBoilers.cs
@@ -0,0 +1,99 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+/// <summary>
+/// 
+/// </summary>
+public class SabotageBoilers : NetworkBehaviour
+{
+    [SerializeField]
+    Image gaugeImage;
+
+    [Header("Setups")]
+    [SerializeField]
+    List<BoilerSabotageOptions> gaugesSetups;
+
+    [SyncVar]
+    int minimum;
+
+    [SyncVar]
+    int maximum;
+
+    [SyncVar(hook = nameof(SetupScreen))]
+    int setupUsed;
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        setupUsed = Random.Range(0, gaugesSetups.Count);
+    }
+
+    void SetupScreen(int oldValue, int newValue)
+    {
+        gaugeImage.sprite = gaugesSetups[newValue].gauge;
+    }
+
+    [Client]
+    public void SetMinimum(string input)
+    {
+        try
+        {
+            var processedInput = int.Parse(input);
+            CmdUpdateMinimum(processedInput);
+        }catch(Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    [Command]
+    void CmdUpdateMinimum(int min)
+    {
+        minimum = min;
+
+        CheckCorrect();
+    }
+
+    [Client]
+    public void SetMaximum(string input)
+    {
+        try
+        {
+            var processedInput = int.Parse(input);
+            CmdUpdateMaximum(processedInput);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
+    }
+
+    [Command]
+    void CmdUpdateMaximum(int max)
+    {
+        maximum = max;
+
+        CheckCorrect();
+    }
+
+    void CheckCorrect()
+    {
+        var isCorrect = false;
+
+        if(maximum <= gaugesSetups[setupUsed].maximum && minimum >= gaugesSetups[setupUsed].minimum)
+        {
+            isCorrect = true;
+        }
+
+        if (isCorrect)
+        {
+            PuzzleCompletion.instance.MarkCompleted(PuzzleId.SabotageBoilerPressure);
+        }
+    }
+}
