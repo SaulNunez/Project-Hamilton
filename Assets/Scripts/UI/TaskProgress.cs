b2dd351d9b9f76271befe450d18dcf20dfe4850a
Saul Nunez <saul.nunez99@gmail.com>
2021-03-12 09:17:02 -0700

    Show users progress in solving puzzles

---
+++ Assets/Scripts/UI/TaskProgress.cs
@@ -0,0 +1,48 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskProgress : NetworkBehaviour
+{
+    [SerializeField]
+    Slider taskSlider;
+
+    [SyncVar(hook = nameof(DoneTasksUpdated))]
+    int doneTasks;
+
+    [SyncVar(hook = nameof(AllTasksUpdated))]
+    int allTasks;
+
+    private void DoneTasksUpdated(int oldValue, int newValue)
+    {
+        taskSlider.value = newValue;
+    }
+
+    private void AllTasksUpdated(int oldValue, int newValue)
+    {
+        taskSlider.maxValue = allTasks;
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        PuzzleCompletion.OnPuzzleCompleted += UpdatePuzzleCount;
+        allTasks = Enum.GetNames(typeof(PuzzleId)).Length;
+    }
+
+    private void UpdatePuzzleCount(PuzzleId obj)
+    {
+        doneTasks++;
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+
+        PuzzleCompletion.OnPuzzleCompleted -= UpdatePuzzleCount;
+    }
+}
