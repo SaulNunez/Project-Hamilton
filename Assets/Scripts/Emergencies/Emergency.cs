2d89c6fd33ba71a4ee3b297b1107ef2a72ab3f4f
Saul Nunez <saul.nunez99@gmail.com>
2021-03-13 18:48:08 -0700

    (WIP) Sabotage

--- Assets/Scripts/Emergencies/Emergency.cs
+++ Assets/Scripts/Emergencies/Emergency.cs
@@ -5,6 +5,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Handles emergency logic, like activating buttons to turn emergency types. 
+/// Also handles wait to activate, and progress; offers countdown event for clients.
+///    
+/// Emergencies have three states:
+/// 1. Waiting to activate
+///     * After voting, there's a wait before them being available
+///     * When a player uses them there's another wait before being available
+/// 2. Available (assasins can activate them)
+/// 3. In progress, when an assasin used them, players are currently scrambling to solve it. Failure to do so finishes the game with victory to the assasins.
+/// 
+/// </summary>
 public class Emergency : NetworkBehaviour
 {
     /// <summary>
@@ -16,14 +28,28 @@ public class Emergency : NetworkBehaviour
     private HubConfig hubConfig;
 
     [SyncVar]
-    public bool isElectricSabotageInProgress;
+    private bool areEmergenciesAvailable = false;
 
     [SyncVar]
-    public bool isBoilerSabotageInProgress;
+    private int timeRemainingOnWaiting;
 
-    public bool IsSabotageActive { get => isElectricSabotageInProgress || isBoilerSabotageInProgress; }
+    [SyncVar]
+    private bool onSabotage = false;
 
-    private int timeRemaing;
+    [SyncVar]
+    private PuzzleId currentActiveSabotage;
+
+    [SyncVar]
+    private int timeRemainingOnEmergency;
+
+    [Header("UI Elements")]
+    [Tooltip("Button for activating boiler sabotage")]
+    [SerializeField]
+    Button boilerSabotage;
+
+    [Tooltip("Button for activating electricity sabotage")]
+    [SerializeField]
+    Button electricitySabotage;
 
     public static Emergency instance = null;
 
@@ -40,6 +66,15 @@ public class Emergency : NetworkBehaviour
     /// </summary>
     public static event Action OnEmergencyResolved;
 
+    private void OnBoilerSabotageAvailabilityChange(bool oldValue, bool newValue)
+    {
+        boilerSabotage.interactable = newValue;
+    }
+
+    private void OnElectricitySabotageAvailabilityChange(bool oldValue, bool newValue)
+    {
+        electricitySabotage.interactable = newValue;
+    }
 
     public override void OnStartServer()
     {
@@ -52,46 +87,76 @@ public class Emergency : NetworkBehaviour
         {
             instance = this;
         }
+
+        VotingManager.OnVotingEnded += StartCooldown;
+        VotingManager.OnVotingStarted += StopSabotageOnVotingStarted;
+
+        StartCooldown();
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+
+        VotingManager.OnVotingEnded -= StartCooldown;
+        VotingManager.OnVotingStarted -= StopSabotageOnVotingStarted;
+    }
+
+    private void StopSabotageOnVotingStarted(int _)
+    {
+        areEmergenciesAvailable = false;
+    }
+
+    /// <summary>
+    /// Set time remaining. Called for starting the cooldown timer.
+    /// </summary>
+    private void StartCooldown()
+    {
+        timeRemainingOnWaiting = hubConfig.secondsOfCooldownForSabotage;
+        Invoke(nameof(CountdownToAvailability), 1);
+    }
+
+    private void CountdownToAvailability()
+    {
+        timeRemainingOnWaiting--;
+        if(timeRemainingOnWaiting > 0) {
+            Invoke(nameof(CountdownToAvailability), 1);
+            return;
+        }
+
+        areEmergenciesAvailable = true;
     }
 
     [Server]
     public void StartEmergency(PuzzleId puzzleId)
     {
-        if (IsSabotageActive)
+        if (!areEmergenciesAvailable)
         {
             return;
         }
 
-        switch (puzzleId)
-        {
-            case PuzzleId.SabotageBoilerPressure:
-                isBoilerSabotageInProgress = true;
-                Invoke(nameof(OnTimeEnded), hubConfig.secondsEmergencyDuration);
-                RpcStartCountdownForEmergency(hubConfig.secondsEmergencyDuration);
-                break;
-            case PuzzleId.SabotageElectricity:
-                isElectricSabotageInProgress = true;
-                RpcStartCountdownForEmergency(hubConfig.secondsEmergencyDuration);
-                Invoke(nameof(OnTimeEnded), hubConfig.secondsEmergencyDuration);
-                break;
-        }
+        areEmergenciesAvailable = false;
+        onSabotage = true;
+
+        currentActiveSabotage = puzzleId;
+        Invoke(nameof(OnTimeEnded), hubConfig.secondsEmergencyDuration);
+        StartCountdownForEmergency();
     }
 
-    [Client]
-    private void Countdown()
+    private void CountdownTimeRemaining()
     {
-        timeRemaing--;
-        if(timeRemaing > 0)
+        timeRemainingOnEmergency--;
+        if(timeRemainingOnEmergency > 0)
         {
-            Invoke(nameof(Countdown), 1f);
+            Invoke(nameof(CountdownTimeRemaining), 1f);
+            return;
         }
+        OnTimeEnded();
     }
 
     [Server]
     private void OnTimeEnded()
     {
-        isBoilerSabotageInProgress = false;
-        isElectricSabotageInProgress = false;
         //TODO: Connect with end game system
     }
 
@@ -99,16 +164,15 @@ public class Emergency : NetworkBehaviour
     public void StopEmergency()
     {
         CancelInvoke(nameof(OnTimeEnded));
-        isBoilerSabotageInProgress = false;
-        isElectricSabotageInProgress = false;
+        onSabotage = false;
+        StartCooldown();
 
         OnEmergencyResolved?.Invoke();
     }
 
-    [ClientRpc]
-    public void RpcStartCountdownForEmergency(int time)
+    public void StartCountdownForEmergency()
     {
-        timeRemaing = time;
-        Invoke(nameof(Countdown), 1f);
+        timeRemainingOnEmergency = hubConfig.secondsEmergencyDuration;
+        Invoke(nameof(CountdownTimeRemaining), 1f);
     }
 }
