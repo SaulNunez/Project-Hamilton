a551fb3a15a99207d545cb9a191ca49172199bce
Saul Nunez <saul.nunez99@gmail.com>
2021-03-19 22:15:10 -0600

    (WIP) Setup end game

---
+++ Assets/Scripts/World/EndGame/SetResultOnGameEnd.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// In charge of controlling stuff that happens when the games end.
+/// 
+/// Actions:
+/// * Shows screen for loosers and winners.
+/// 
+/// Results:
+/// * Programmers win when they vote all assasins
+/// * Programmers win when they finish all tasks
+/// * Assasins win when there's the same number of programmers as of them, either by killing or by voting out
+/// * Assasins win when players don't fix a sabotage on time
+/// </summary>
+public class SetWinnerLoserOnGameEnd : NetworkBehaviour
+{
+    private EndGameResult? result = null;
+
+    public enum EndGameResult
+    {
+        AssasinsWin,
+        ProgrammersWin
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        Emergency.OnPlayersCouldntStopEmergency += OnPlayersCouldntStopEmergency;
+    }
+
+    private void OnPlayersCouldntStopEmergency()
+    {
+        result = EndGameResult.AssasinsWin;   
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+    }
+}
