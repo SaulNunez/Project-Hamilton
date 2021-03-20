df41a36c672bd2b8d0957a46c680efddfbc142c3
Saul Nunez <saul.nunez99@gmail.com>
2021-03-19 20:17:16 -0600

    Add documentation

--- Assets/Scripts/Puzzles/SabotageBoilers/BoilerSabotageOptions.cs
+++ Assets/Scripts/Puzzles/SabotageBoilers/BoilerSabotageOptions.cs
@@ -3,6 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Used by boiler sabotage component.
+/// 
+/// There's a variety of gauges. This is used to provided several gauges configurations.
+/// </summary>
 [Serializable]
 public class BoilerSabotageOptions
 {
