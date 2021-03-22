e7caa779f73ceba1216493cd745a9355d3f29ebd
Saul Nunez <saul.nunez99@gmail.com>
2021-03-22 16:36:34 -0600

    Make sequence puzzle add progress on server

---
+++ Assets/Scripts/Puzzles/Sequence/SequencePuzzle.cs
@@ -0,0 +1,43 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Sequence puzzle config
+/// 
+/// Currently messy, there's no way to make sure on server this has been completed correctly.
+/// </summary>
+public class SequencePuzzle : NetworkBehaviour
+{
+    [Tooltip("Puzzle we are controlling")]
+    [SerializeField]
+    SequenceGrid sequencePuzzle;
+    
+    [Tooltip("Sequence puzzle to mark as solved")]
+    [SerializeField]
+    PuzzleId sequenceId;
+
+    /// <summary>
+    /// Call after sequence has been completed to count it on server
+    /// </summary>
+    [Client]
+    public void SetPuzzleComplete()
+    {
+        CmdCompletePuzzle();
+    }
+
+    [Command(ignoreAuthority =true)]
+    void CmdCompletePuzzle(NetworkConnectionToClient sender = null)
+    {
+        PuzzleCompletion.instance.MarkCompleted(sequenceId);
+        TargetClosePuzzle(sender);
+    }
+
+    [TargetRpc]
+    void TargetClosePuzzle(NetworkConnection target)
+    {
+        ShowPuzzle.instance.ClosePuzzles();
+    }
+}
