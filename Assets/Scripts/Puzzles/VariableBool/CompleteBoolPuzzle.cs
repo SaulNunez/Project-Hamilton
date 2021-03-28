b9252061dc90af99b877b5424b12ed655953490a
Saul Nunez <saul.nunez99@gmail.com>
2021-03-27 21:59:03 -0600

    Changed bool puzzle to use button

--- Assets/Scripts/Puzzles/VariableBool/CompleteBoolPuzzle.cs
+++ Assets/Scripts/Puzzles/VariableBool/CompleteBoolPuzzle.cs
@@ -2,12 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Used for puzzle regarding booleans. Uses a lever as a way to teach concept of booleans.
+/// </summary>
 public class CompleteBoolPuzzle : NetworkBehaviour
 {
-    public void CompletePuzzle(bool toggleState)
+    [Tooltip("Sprite to use for on state")]
+    [SerializeField]
+    Sprite onLever;
+
+    [Tooltip("UI element for puzzle that show current lever state")]
+    [SerializeField]
+    Image leverElement;
+
+    /// <summary>
+    /// For UI, it sends a server command to complete and sets button to show the lever has been activated
+    /// </summary>
+    public void CompletePuzzle()
     {
-        CmdSendPuzzleState(toggleState);
+        CmdSendPuzzleState(true);
+        leverElement.sprite = onLever;
     }
 
     [Command(ignoreAuthority = true)]
