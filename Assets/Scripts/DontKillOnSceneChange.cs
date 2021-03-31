78ccffab7a64660a3e033c3be020728ce54920de
Saul Nunez <saul.nunez99@gmail.com>
2021-03-31 01:05:36 -0600

    Lobby setup config

---
+++ Assets/Scripts/DontKillOnSceneChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Utility that can be added to any gameobject that needs to persist scene changes.
+/// </summary>
+public class DontKillOnSceneChange : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/UI/Lobby/LobbyOptions/LobbyOptionsUiConnections.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Future work, handles updating UI state with hub config in lobby to allow setup
+/// </summary>
+public class LobbyOptionsUiConnections : MonoBehaviour
+{
+    [SerializeField]
+    Slider impostorSlider;
+
+    [SerializeField]
+    Slider reunionCooldown;
+
+    [SerializeField]
+    Slider sabotageDuration;
+
+    [SerializeField]
+    Slider actionDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
