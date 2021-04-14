b1bc8146df87309a42d6dc23381c793ce2d92cb1
Saul Nunez <saul.nunez99@gmail.com>
2021-04-14 01:42:44 -0600

    (Not tested) Camera allows override targets

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -2,25 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls camera position to follow the player or an override target
+/// </summary>
 public class CameraControl : MonoBehaviour
 {
+    [Tooltip("Seconds to reach target, used by smoothing")]
     [SerializeField]
     [Range(0.01f, 10f)]
     float maxReachTime = 1.0f;
 
+    /// <summary>
+    /// Player to target, only used to read `transform.position`
+    /// </summary>
     [HideInInspector]
     public Transform target;
 
     float xCurrentSpeed = 0, yCurrentSpeed = 0;
 
+    /// <summary>
+    /// If is targeting a specific position, or if disabled the player
+    /// </summary>
+    bool onOverride = false;
+
+    /// <summary>
+    /// Position to target on override
+    /// </summary>
+    Vector2 overrideTarget;
+
     void Update(){
         if(target != null) {
             transform.position = new Vector3(
-                Mathf.SmoothDamp(transform.position.x, target.position.x, 
+                Mathf.SmoothDamp(transform.position.x, onOverride? overrideTarget.x : target.position.x, 
                                     ref xCurrentSpeed, maxReachTime),
-                Mathf.SmoothDamp(transform.position.y, target.position.y, 
+                Mathf.SmoothDamp(transform.position.y, onOverride? overrideTarget.y : target.position.y, 
                                     ref yCurrentSpeed, maxReachTime), 
                 transform.position.z);
         }
+    }
+
+    /// <summary>
+    /// Enables override targeting
+    /// </summary>
+    public void MoveCameraToTarget(Vector2 newTarget)
+    {
+        overrideTarget = newTarget;
+        onOverride = true;
+    }
+
+    /// <summary>
+    /// Disables override targeting, making camera follow player
+    /// </summary>
+    public void FollowPlayer()
+    {
+        onOverride = false;
     }
 }
