5e8f8f495aab6a7da4f9626083a689c308018a48
Saul Nunez <saul.nunez99@gmail.com>
2021-05-10 23:22:01 -0600

    Hide hud in client

---
+++ Assets/Scripts/Multiplayer/HideGuiIfNotEditor.cs
@@ -0,0 +1,17 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkManagerHUD))]
+public class HideGuiIfNotEditor : MonoBehaviour
+{
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            var hud = GetComponent<NetworkManagerHUD>();
+            hud.showGUI = false;
+        }
+    }
+}
