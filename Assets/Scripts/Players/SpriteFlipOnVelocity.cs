d495ac7fed2bcc7fcac017c6bb886c6ac5b3b09f
Saul Nunez <saul.nunez99@gmail.com>
2021-05-11 23:05:01 -0600

    Flip sprite on players direction

---
+++ Assets/Scripts/Players/SpriteFlipOnVelocity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlipOnVelocity : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    new Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(rigidbody2D.velocity.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        } else if(rigidbody2D.velocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
