6cb02d24d8b8362c20a23339b4f276da8823c388
Saul Nunez <saul.nunez99@gmail.com>
2021-05-12 17:19:06 -0600

    Don't move player when UI is shown

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -9,14 +9,41 @@ public class PlayerControls : NetworkBehaviour
     public float timeToMax = 0.4f;
     public new Rigidbody2D rigidbody2D;
 
+    bool waitToCloseUi = false;
+
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+
+        PuzzleUI.OnOpenPuzzleInClient += OnUIShown;
+        PuzzleUI.OnClosePuzzleInClient += OnUiClose;
+    }
+
+    private void OnUiClose()
+    {
+        waitToCloseUi = false;
+    }
+
+    private void OnUIShown()
+    {
+        waitToCloseUi = true;
+    }
+
     void FixedUpdate()
     {
-        if (hasAuthority)
+        if (hasAuthority && !waitToCloseUi)
         {
             var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             var y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
             rigidbody2D.MovePosition(new Vector2(x + transform.position.x, y + transform.position.y));
         }
+    }
+
+    public override void OnStopAuthority()
+    {
+        base.OnStopAuthority();
+
+
     }
 }
