1855deb9addb0c3d61be9bcbd2f9e20b1b55e00c
Saul Nunez <saul.nunez99@gmail.com>
2021-05-14 21:10:47 -0600

    Show role of player on game start

---
+++ Assets/Scripts/Players/ShowPlayerRoleOnStart.cs
@@ -0,0 +1,25 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+[RequireComponent(typeof(Killing))]
+public class ShowPlayerRoleOnStart : NetworkBehaviour
+{
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+
+        var killing = GetComponent<Killing>();
+        if (killing.canKill)
+        {
+            Messages.instance.ShowMessage("Eres un asesino, mata a los programadores para ganar");
+        } else
+        {
+            Messages.instance.ShowMessage("Eres un programador, resuelve los acertijos alrededor del mapa para ganar");
+        }
+    }
+}
