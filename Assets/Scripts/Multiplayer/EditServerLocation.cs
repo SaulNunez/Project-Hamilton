9cb963a95d48eaac235c48d8528e30580411957e
Saul Nunez <saul.nunez99@gmail.com>
2021-08-13 23:27:42 -0600

    Permitir entrar servidor a usar en juego y simplificar cliente

---
+++ Assets/Scripts/Multiplayer/EditServerLocation.cs
@@ -0,0 +1,46 @@
+﻿using Mirror;
+using Mirror.SimpleWeb;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EditServerLocation : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField serverAddressInput;
+    [SerializeField]
+    TMP_InputField portInput;
+
+    [SerializeField]
+    NetworkManager networkManager;
+
+    [SerializeField]
+    SimpleWebTransport transport;
+
+    void Start()
+    {
+        serverAddressInput.text = networkManager.networkAddress;
+        serverAddressInput.onEndEdit.AddListener(UpdateServerAddress);
+
+        portInput.text = transport.port.ToString();
+        portInput.onEndEdit.AddListener(UpdatePort);
+    }
+
+    void UpdateServerAddress(string newValue)
+    {
+        networkManager.networkAddress = newValue;
+    }
+    
+    void UpdatePort(string newValue)
+    {
+        if (ushort.TryParse(newValue, out ushort port))
+        {
+            transport.port = port;
+        }
+        else
+        {
+            portInput.text = transport.port.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Multiplayer/SetPortOnWebGl.cs
@@ -0,0 +1,24 @@
+﻿using Mirror.SimpleWeb;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPortOnWebGl : MonoBehaviour
+{
+    [SerializeField]
+    ushort newPort;
+
+    SimpleWebTransport transport;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transport = GetComponent<SimpleWebTransport>();
+
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            transport.port = newPort;
+        }
+    }
+
+}
