72fe3dbf0f7ce1bf83544db6da7c0761e1ec67be
Saul Nunez <saul.nunez99@gmail.com>
2021-08-15 22:29:45 -0600

    Iniciar cliente con conexion segura websocket si entran por https

---
+++ Assets/Scripts/Multiplayer/UseWSSInHttps.cs
@@ -0,0 +1,22 @@
+﻿using Mirror.SimpleWeb;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseWSSInHttps : MonoBehaviour
+{
+    SimpleWebTransport transport;
+
+    void Start()
+    {
+        transport = GetComponent<SimpleWebTransport>();
+
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            var serverUrlUri = new Uri(Application.absoluteURL);
+
+            transport.clientUseWss = serverUrlUri.Scheme == Uri.UriSchemeHttps;
+        }
+    }
+}
