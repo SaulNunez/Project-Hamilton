85529e9eefe8257773cb016e697a0a60a46dbb11
Saul Nunez <saul.nunez99@gmail.com>
2021-08-19 16:46:50 -0600

    Despawn skeleton after voting

---
+++ Assets/Scripts/Death/DespawnAfterVoting.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+
+public class DespawnAfterVoting : NetworkBehaviour
+{
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        VotingManager.OnVotingEnded += RemoveDeadPlayerRemains;
+    }
+
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+
+        VotingManager.OnVotingEnded -= RemoveDeadPlayerRemains;
+    }
+
+    void RemoveDeadPlayerRemains()
+    {
+        NetworkServer.Destroy(gameObject);
+    }
+}
