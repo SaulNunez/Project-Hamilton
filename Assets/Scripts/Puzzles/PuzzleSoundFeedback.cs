68bf1cb5da6287efd466ce48a3ff914dd4ca09b2
Saul Nuñez <saul.nunez99@gmail.com>
2021-09-08 20:24:48 -0600

    Agregar efectos a sonar para retroalimentacion de puzle

---
+++ Assets/Scripts/Puzzles/PuzzleSoundFeedback.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSoundFeedback : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource wrongAnswerAudioSource;
+    [SerializeField]
+    AudioSource correctAnswerAudioSource;
+
+    public static PuzzleSoundFeedback instance = null;
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void WrongAnswer() => wrongAnswerAudioSource.Play();
+
+    public void CorrectAnswer() => correctAnswerAudioSource.Play();
+}
