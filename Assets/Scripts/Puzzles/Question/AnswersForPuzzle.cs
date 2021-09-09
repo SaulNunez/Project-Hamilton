40c10f3e48c3b65f869069235b44629a9e73b151
Saul Nuñez <saul.nunez99@gmail.com>
2021-09-09 14:23:29 -0600

    Cambiar puzzle do while a una pregunta de ciclos

---
+++ Assets/Scripts/Puzzles/Question/AnswersForPuzzle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class AnswersForPuzzle
+{
+    public Button buttonForSelectingAnswer;
+    public bool isACorrectAnswer;
+}
---
+++ Assets/Scripts/Puzzles/Question/QuestionPuzzle.cs
@@ -0,0 +1,40 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionPuzzle : PuzzleBase
+{
+    [SerializeField]
+    PuzzleId puzzleId;
+
+    [SerializeField]
+    List<AnswersForPuzzle> answers = new List<AnswersForPuzzle>();
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        for(int i=0; i < answers.Count; i++)
+        {
+            int index = i;
+            answers[index].buttonForSelectingAnswer.onClick.AddListener(() => {
+                CmdCheckAnswer(index);
+            });
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    void CmdCheckAnswer(int answerIndex, NetworkConnectionToClient sender = null)
+    {
+        if (answers[answerIndex].isACorrectAnswer)
+        {
+            PuzzleCompletion.instance.MarkCompleted(puzzleId, sender.identity);
+            TargetClosePuzzle(sender);
+            TargetRunCorrectFeedback(sender);
+        } else
+        {
+            TargetRunWrongFeedback(sender);
+        }
+    }
+}
