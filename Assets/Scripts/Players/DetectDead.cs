56ed68a93924f8f78d61196405d0d6760c4473ef
Saul Nuñez <saul.nunez99@gmail.com>
2021-09-28 23:38:29 -0600

    Detectar personas muertas depende del estado de vida del jugador, la gente muerta no puede reportar

--- Assets/Scripts/Players/DetectDead.cs
+++ Assets/Scripts/Players/DetectDead.cs
@@ -10,6 +10,7 @@ public class DetectDead : NetworkBehaviour
     private LayerMask deadPlayersLayers;
 
     HubConfig config;
+    Die deadStateComponent;
 
     public override void OnStartServer()
     {
@@ -17,6 +18,8 @@ public class DetectDead : NetworkBehaviour
 
         var hubConfigGO = GameObject.FindGameObjectWithTag(Tags.HubConfig);
         config = hubConfigGO.GetComponent<HubConfig>();
+
+        deadStateComponent = GetComponent<Die>();
     }
 
     public override void OnStartClient()
@@ -25,6 +28,8 @@ public class DetectDead : NetworkBehaviour
 
         var hubConfigGO = GameObject.FindGameObjectWithTag(Tags.HubConfig);
         config = hubConfigGO.GetComponent<HubConfig>();
+
+        deadStateComponent = GetComponent<Die>();
 
         GameUI.OnReportClick += CmdReport;
     }
@@ -41,8 +46,11 @@ public class DetectDead : NetworkBehaviour
         if (isClient)
         {
             var found = Physics2D.OverlapCircle(transform.position, config.actDistance, deadPlayersLayers);
+            var dead = deadStateComponent.IsDead;
 
-            GameUI.Instance.CanInteractWithReportButton = found;
+            var canReport = found && !dead;
+
+            GameUI.Instance.CanInteractWithReportButton = canReport;
         }
     }
 
@@ -50,7 +58,11 @@ public class DetectDead : NetworkBehaviour
     private void CmdReport()
     {
         var found = Physics2D.OverlapCircle(transform.position, config.actDistance, deadPlayersLayers);
-        if (found)
+        var dead = deadStateComponent.IsDead;
+
+        var canReport = found && !dead;
+
+        if (canReport)
         {
             var votingManagerGameObject = GameObject.FindGameObjectWithTag(Tags.VotingManager);
             var votingManager = votingManagerGameObject.GetComponent<VotingManager>();
